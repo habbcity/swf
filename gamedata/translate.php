3b83aa834e2dc05750d39747f10525be007fcaa8
Habb <vps@habb.city>
2022-02-13 22:23:13 -0300

    first commit merge

---
+++ gamedata/translate.php
@@ -0,0 +1,64 @@
+<?php
+
+$start = microtime(true);
+
+ini_set('display_errors', false);
+ini_set('display_startup_errors', false);
+
+if (!file_exists('./furnidata.xml')) {
+    throw new RuntimeException('furnidata.xml is does not exist or is not readable');
+}
+
+if (!file_exists('./furnidata_br.xml')) {
+    throw new RuntimeException('furnidata_br.xml is does not exist or is not readable');
+}
+
+/* edit: Download latest furnidata files */
+exec('wget "https://habbo.com/gamedata/furnidata_xml/1" -O "' . './furnidata.xml"');
+exec('wget "https://habbo.com.br/gamedata/furnidata_xml/1" -O "' . './furnidata_br.xml"');
+/* end edit */
+
+$furnidata = new SimpleXMLElement(file_get_contents('./furnidata.xml'));
+$translate = new SimpleXMLElement(file_get_contents('./furnidata_br.xml'));
+$translate = array_merge_recursive(
+    (array) $translate->roomitemtypes,
+    (array) $translate->wallitemtypes
+);
+
+foreach ($translate['furnitype'] as $furni) {
+    $file         = (string) $furni->attributes()['classname'];
+    $trans[$file] = [
+        $furni->name,
+        $furni->description,
+    ];
+}
+
+foreach ($furnidata->roomitemtypes->furnitype as $furni) {
+    $file = (string) $furni->attributes()['classname'];
+
+    if (!isset($trans[$file])) {
+        continue;
+    }
+
+    list($name, $desc) = $trans[$file];
+
+    $furni->name        = $name;
+    $furni->description = $desc;
+}
+
+foreach ($furnidata->wallitemtypes->furnitype as $furni) {
+    $file = (string) $furni->attributes()['classname'];
+
+    if (!isset($trans[$file])) {
+        continue;
+    }
+
+    list($name, $desc) = $trans[$file];
+
+    $furni->name        = $name;
+    $furni->description = $desc;
+}
+
+$furnidata->asXML('./furnidata.xml');
+
+echo 'Finished, execution time: ' . (microtime(true) - $start) . ' seconds';
