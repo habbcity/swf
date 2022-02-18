70da15124e08b145d1880d0b8f38a9e26bf38bba
jvsdr <nexushp@hotmail.com>
2022-02-18 00:17:54 -0300

    update furnidata

---
+++ gamedata/update.bat
@@ -0,0 +1,4 @@
+wget "http://habbo.com/gamedata/furnidata_xml/1" -O "furnidata.xml" 
+wget "http://habbo.com.br/gamedata/furnidata_xml/1" -O "furnidata_br.xml" 
+wget "http://habbo.com/gamedata/furnidata_json/1" -O "furnidata.json" 
+wget "http://habbo.com.br/gamedata/furnidata_json/1" -O "furnidata_br.json" 
